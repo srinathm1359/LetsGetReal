68bf749f5e66c1af3e29bef76e4c0cd02195ee5a
Arvind Junior <arvindjunior@Srinath-Air.lan>
2020-11-22 22:31:05 -0500

    Tested equals()

--- Tester.java
+++ Tester.java
@@ -1,5 +1,17 @@
 public class Tester {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    //Testing equals() for RealNumber()
+    RealNumber newReal1 = new RealNumber(0.0);
+    RealNumber newReal2 = new RealNumber(0.0);
+    RealNumber nonZero = new RealNumber(1.0);
+    RealNumber notOne = new RealNumber(1.0001);
+    RealNumber One = new RealNumber(1.0);
+    RealNumber alsoOne = new RealNumber(1.000009);
+    System.out.println(newReal1.equals(newReal2));//true
+    System.out.println(newReal1.equals(nonZero));//false
+    System.out.println(nonZero.equals(newReal1));//false
+    System.out.println(nonZero.equals(notOne));//false
+    System.out.println(nonZero.equals(One));//true
+    System.out.println(nonZero.equals(alsoOne));//true
   }
 }
