deb03b522b8d55bcd48402a07c135c418bd81ee4
Arvind Junior <arvindjunior@Srinath-Air.lan>
2020-11-23 03:01:01 -0500

    Added divide()

--- RationalNumber.java
+++ RationalNumber.java
@@ -112,7 +112,7 @@ public class RationalNumber extends RealNumber
   *Return a new RationalNumber that is the this divided by the other
   */
   public RationalNumber divide(RationalNumber other){
-    return null;
+    return (this.multiply(other.reciprocal()));
   }
 
   /**
