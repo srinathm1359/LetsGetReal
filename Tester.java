a3517aad80418ea43d10f0cc85ba60be4fd5a21e
Arvind Junior <arvindjunior@Srinath-Air.lan>
2020-11-23 02:26:07 -0500

    Updated Tester.java for neatness

--- Tester.java
+++ Tester.java
@@ -2,6 +2,7 @@ public class Tester {
   public static void main(String[] args) {
     //Tests for RealNumber()
     //Testing equals() for RealNumber()
+    System.out.println("\nTesting equals() for RealNumber()");
     RealNumber newReal1 = new RealNumber(0.0);
     RealNumber newReal2 = new RealNumber(0.0);
     RealNumber nonZero = new RealNumber(1.0);
@@ -15,22 +16,27 @@ public class Tester {
     System.out.println(nonZero.equals(One));//true
     System.out.println(nonZero.equals(alsoOne));//true
     //Testing add() for RealNumber()
+    System.out.println("\nTesting add() for RealNumber()");
     System.out.println(One.add(alsoOne));
     System.out.println(One.add(notOne));
     System.out.println(One.add(newReal1));
     //Testing multiply() for RealNumber()
+    System.out.println("\nTesting multiply() for RealNumber()");
     System.out.println(One.multiply(alsoOne));
     System.out.println(notOne.multiply(alsoOne));
     //Testing divide() for RealNumber()
+    System.out.println("\nTesting divide() for RealNumber()");
     System.out.println(One.divide(alsoOne));
     //System.out.println(One.divide(newReal1));
     //Throws RuntimeException
     //Testing subtract() for RealNumber()
+    System.out.println("\nTesting subtract() for RealNumber()");
     System.out.println(One.subtract(nonZero));
     System.out.println(One.subtract(newReal1));
     System.out.println(alsoOne.subtract(notOne));
     //Tests for RationalNumber()
     //Testing toString() and RationalNumber()
+    System.out.println("\nTesting toString() and RationalNumber()");
     RationalNumber OneRational = new RationalNumber(8,8);
     RationalNumber overZero = new RationalNumber(2,0);
     RationalNumber threeFourths = new RationalNumber(3,4);
@@ -43,7 +49,8 @@ public class Tester {
     System.out.println(sixEighths);
     System.out.println(negativeThreeFourths);
     System.out.println(Three);
-    //Testing getValue()
+    //Testing getValue() for RationalNumber()
+    System.out.println("\nTesting getValue() for RationalNumber()");
     System.out.println(OneRational.getValue());
     System.out.println(overZero.getValue());
     System.out.println(threeFourths.getValue());
